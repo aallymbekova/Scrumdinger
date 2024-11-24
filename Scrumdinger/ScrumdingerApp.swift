b1efa02308b86cd156111dcd80dd8e07edde995c
aallymbekova <alymbekovaaliya@icloud.com>
2024-11-24 23:27:10 +0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Aliya Alymbekova on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
